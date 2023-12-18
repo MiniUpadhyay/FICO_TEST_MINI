bba22df45ebdbc61520df6b6bfac5030bca8e233
mini.upadhyay07@gmail.com <mini.upadhyay07@gmail.com>
2023-12-18 15:07:14 +0000

    commit order_stg.sql

---
+++ models/FICO_DBT_TEST/order_stg.sql
@@ -0,0 +1,36 @@
+{{
+  config(
+    materialized='incremental',
+    unique_key='ORDERID'
+  )
+}}
+
+with order_stg as (
+select orderid,
+orderdate,
+customerid,
+employeeid,
+storeid,
+status as statuscd,
+case 
+    when status = '01' then 'In Progress'
+    when status = '02' then 'Completed'
+    when status = '03' then 'Cancelled'
+    else null
+end as statusdesc,
+case
+    when storeid = 67567 then 'Online'
+    else 'In-store'
+end as order_channel,
+_fivetran_synced,
+current_timestamp as dbt_updated_at
+from raw_fico.fico_sch.ORDERS)
+select orderid,orderdate,customerid,employeeid,
+storeid,statuscd,statusdesc,
+order_channel,_fivetran_synced,dbt_updated_at from order_stg
+
+{% if is_incremental() %}
+
+  where _fivetran_synced >= (select max(dbt_updated_at) from {{ this }})
+
+{% endif %}
