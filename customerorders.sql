cd06aea63a40a1021b5fbac225c27df987dfbc3b
mini.upadhyay07@gmail.com <mini.upadhyay07@gmail.com>
2023-12-18 15:25:28 +0000

    Commit customerorder

--- customerorders.sql
+++ customerorders.sql
@@ -1,7 +1,7 @@
 {{config(materilized='table')}}
 with customerorders as (
     SELECT c.customerid, concat(c.firstname,' ', c.lastname) as customername, count(o.orderid) as No_of_Orders 
-    from raw_fico.fico_sch.customers c
+    from DATA_LAKE_FICO.RAW_SCH.customers c
 join DATA_LAKE_FICO.RAW_SCH.orders o on c.customerid=o.customerid
 group by c.customerid, customername order by no_of_orders desc
 )
