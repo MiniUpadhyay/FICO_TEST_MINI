c7dbe35f1e3329e240111bbab0c8599851cf141d
Mini Upadhyay <mini.upadhyay07@gmail.com>
2023-12-20 08:55:31 +0000

    commit

--- models/FICO_DBT_TEST/testfile1.sql
+++ models/FICO_DBT_TEST/testfile1.sql
@@ -2,6 +2,6 @@ with testfile1 as (
     SELECT c.customerid, c.firstname, c.lastname, concat(c.firstname,' ', c.lastname) as customername, count(o.orderid) as No_of_Orders 
     from DATA_LAKE_FICO.RAW_SCH.customers c
 join DATA_LAKE_FICO.RAW_SCH.orders o on c.customerid=o.customerid
-group by c.customerid, customername order by no_of_orders desc
+group by c.customerid, firstname, lastname,customername order by no_of_orders desc
 )
 select customerid,customername,No_of_Orders from testfile1
