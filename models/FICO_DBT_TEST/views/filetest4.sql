9df2742d9b9413d02cea2468575860a0719c7d4c
Mini Upadhyay <mini.upadhyay07@gmail.com>
2023-12-20 12:33:32 +0000

    nk

--- models/FICO_DBT_TEST/views/filetest4.sql
+++
@@ -1,7 +0,0 @@
-with testfile4 as (
-    SELECT c.customerid, c.firstname, c.lastname, concat(c.firstname,' ', c.lastname) as customername, count(o.orderid) as No_of_Orders 
-    from DATA_LAKE_FICO.RAW_SCH.customers c
-join {{ ref('order_stg') }} o on c.customerid=o.customerid
-group by c.customerid, firstname, lastname,customername order by no_of_orders desc
-)
-select customerid,customername,No_of_Orders from testfile4
--- models/FICO_DBT_TEST/views/testfile4.sql
+++ models/FICO_DBT_TEST/views/testfile4.sql
@@ -1,7 +1,7 @@
 with testfile4 as (
     SELECT c.customerid as custid2, c.firstname, c.lastname, concat(c.firstname,' ', c.lastname) as customername, count(o.orderid) as No_of_Orders 
-    from DATA_LAKE_FICO.RAW_SCH.customers c
-join {{ ref('order_stg') }} o on c.customerid=o.customerid
+    from {{ source('raw', 'customers') }} c
+join {{ source('raw', 'orders') }}  o on c.customerid=o.customerid
 group by 1, firstname, lastname,customername order by no_of_orders desc
 )
 select customerid,customername,No_of_Orders from testfile4
